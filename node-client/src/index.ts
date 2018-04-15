960da8d764f06c0bc0ceaedf29ebc1854c0a0551
Alex Clemmer <clemmer.alexander@gmail.com>
2018-04-15 15:48:50 -0700

    Refer to clients by minor version

--- node-client/src/index.ts
+++ node-client/src/index.ts
@@ -1,5 +1,6 @@
-import * as client_1_7_4 from "./client-1.7.4";
-export {client_1_7_4};
-
-import * as client_1_9_6 from "./client-1.7.4";
-export {client_1_9_6};
+export * from './client-1.9/config';
+export * from './client-1.9/api';
+export * from './client-1.9/types';
+export * from './client-1.9/attach';
+export * from './client-1.9/watch';
+export * from './client-1.9/exec';
